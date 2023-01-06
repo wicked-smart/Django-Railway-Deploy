4d22751bb9b82863492c1926ab9b28e8f729c41f
wicked-smart <marzaanvan@gmail.com>
2023-01-06 11:34:04 +0530

    Add CSRF_TRUSTED_DOMAINS

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
@@ -137,3 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 AUTH_USER_MODEL = 'hello.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Trusted Origin issue
+CSRF_TRUSTED_ORIGINS = ['https://django-railway-deploy-production.up.railway.app']
