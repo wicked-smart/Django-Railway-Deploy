ec2db266e6657c826c6e3970b894646a7f7eddb2
wicked-smart <marzaanvan@gmail.com>
2023-01-06 11:26:42 +0530

    run collectstatic and Add STATIC_ROOT in settings.py

--- hello/urls.py
+++ hello/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.conf.urls.static import static
 from .import views
+from mysite import settings
 
 
 urlpatterns = [
     path('', views.index, name="index")
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
