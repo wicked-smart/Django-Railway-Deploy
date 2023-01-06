56ba5898998b768461b1fd300897b0369347b204
wicked-smart <marzaanvan@gmail.com>
2023-01-06 14:38:05 +0530

    add Passengers Model

--- hello/admin.py
+++ hello/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Airport, Flight
+from .models import User, Airport, Flight, Passengers
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Airport)
 admin.site.register(Flight)
+admin.site.register(Passengers)
--- hello/models.py
+++ hello/models.py
@@ -20,3 +20,11 @@ class Flight(models.Model):
 
     def __str__(self):
         return f"{self.origin} to {self.destination}"
+
+class Passengers(models.Model):
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    flight = models.ManyToManyField(Flight, related_name="passengers")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
