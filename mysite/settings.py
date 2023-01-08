a7861da146d81fcd317bda81e49f979134fb22d3
wicked-smart <marzaanvan@gmail.com>
2023-01-08 13:21:30 +0530

    change static folder location

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "hello/static")
 
 
 # Default primary key field type
