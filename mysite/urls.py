3fbd79d0bc281d5223f93fb1b794e75d4471f57e
wicked-smart <marzaanvan@gmail.com>
2023-01-10 13:34:32 +0530

    Add single page app

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hello.urls'))
+    path('hello/', include('hello.urls')),
+    path('singlepageapp/', include('singlepageapp.urls'))
 ]
