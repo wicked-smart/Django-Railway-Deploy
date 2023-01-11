3ab9a795ad3feb1db4f907b0b51b4bd24643f3a4
wicked-smart <marzaanvan@gmail.com>
2023-01-11 12:35:45 +0530

    add changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include('hello.urls')),
     path('singlepageapp/', include('singlepageapp.urls'))
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
