5042fdc51e562fca8b597bc3f6f5f9478bc7499a
wicked-smart <marzaanvan@gmail.com>
2023-01-12 09:51:24 +0530

    testing singlepageapp

--- singlepageapp/urls.py
+++ singlepageapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="singleapp-index")
+    path("", views.index, name="singleapp-index"),
+    path("section/<int:section_id>", views.section, name="singlepageapp-section")
 ]
--- singlepageapp/views.py
+++ singlepageapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 # Create your views here.
 
 def index(request):
-    return render(request, "singlepageapp/index.html")
+    return (request, "singlepageapp/index.html")
+
+texts = ['section1', 'section2', 'section3']
+
+def section(request, section_id):
+    if(section_id >= 1 and section_id <= 3):
+        return HttpResponse(texts[section_id-1])
+    else:
+        raise Http404("No such section exists!")
