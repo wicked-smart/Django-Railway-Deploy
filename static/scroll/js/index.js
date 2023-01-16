7f99feee013761d6f8762b4c8dd448e54d2a634a
wicked-smart <marzaanvan@gmail.com>
2023-01-16 10:20:25 +0530

    run collecstattic

---
+++ static/scroll/js/index.js
@@ -0,0 +1,25 @@
+// create posts 
+function createPosts(num){
+    var parent = document.querySelector('#posts')
+    for(let i=1; i<=num; i++){
+        let div = document.createElement('div')
+        var bar = "posts #" + i 
+        div.innerHTML = `<h2> ${bar} </h2><br>`
+        div.classList.add('post')
+        parent.append(div)
+    }
+
+}
+
+
+// call createPosts on Document load
+document.addEventListener("DOMContentLoaded", function(){
+    createPosts(10)
+
+    window.onscroll = () => {
+        if(window.scrollY + window.innerHeight >= document.body.offsetHeight)
+            document.querySelector('body').style.background = 'green'
+        else
+            document.querySelector('body').style.background = 'white'
+    }
+})
